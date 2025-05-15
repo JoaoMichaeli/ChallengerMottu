ffe3531a4eaaf61358f385da67b7fcfc7c1fdf5b
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-05-15 17:00:45 -0300

    repository

---
+++ primeira sprint/Java/visionhive/src/main/java/com/visionhive/visionhive/repository/BranchRepository.java
@@ -0,0 +1,7 @@
+package com.visionhive.visionhive.repository;
+
+import com.visionhive.visionhive.model.Branch;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BranchRepository extends JpaRepository<Branch, Long> {
+}
---
+++ primeira sprint/Java/visionhive/src/main/java/com/visionhive/visionhive/repository/MotorcycleRepository.java
@@ -0,0 +1,6 @@
+package com.visionhive.visionhive.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MotorcycleRepository extends JpaRepository<MotorcycleRepository, Long> {
+}
