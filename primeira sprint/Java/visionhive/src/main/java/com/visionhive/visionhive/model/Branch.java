ddb04262301b3390e7dd1d149022859ed95e4a1d
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-05-15 16:55:36 -0300

    model

---
+++ primeira sprint/Java/visionhive/src/main/java/com/visionhive/visionhive/model/Branch.java
@@ -0,0 +1,30 @@
+package com.visionhive.visionhive.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Branch {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome da filial não pode estar em branco")
+    private String name;
+
+    @NotBlank(message = "O bairro não pode estar em branco")
+    private String bairro;
+
+    @OneToOne
+    @JsonIgnore
+    private Patio patio;
+}
---
+++ primeira sprint/Java/visionhive/src/main/java/com/visionhive/visionhive/model/MotorcycleModel.java
@@ -0,0 +1,7 @@
+package com.visionhive.visionhive.model;
+
+public enum MotorcycleModel {
+    MottuSport,
+    MottuE,
+    MottuPop
+}
